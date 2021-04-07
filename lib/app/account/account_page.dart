057b9976cb01f9b630e14064bda03b6d12aeed5e
Sarp Solakoglu <sosolakoglu@gmail.com>
2021-04-07 20:25:50 +0100

    Section 23

--- lib/app/account/account_page.dart
+++ lib/app/account/account_page.dart
@@ -1,10 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:time_tracker_app/common_widgets/avatar.dart';
 import 'package:time_tracker_app/common_widgets/show_alert_dialog.dart';
 import 'package:time_tracker_app/services/auth.dart';
 
 class AccountPage extends StatelessWidget {
-
   Future<void> _confirmSignOut(BuildContext context) async {
     final didRequestSignOut = await showAlertDialog(
       context,
@@ -29,6 +30,7 @@ class AccountPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final auth = Provider.of<AuthBase>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text('Account'),
@@ -44,9 +46,34 @@ class AccountPage extends StatelessWidget {
             ),
           ),
         ],
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(130),
+          child: _buildUserInfo(auth.currentUser),
+        ),
       ),
       body: Container(),
     );
   }
 
+  Widget _buildUserInfo(User? user) {
+    return Column(
+      children: [
+        Avatar(
+          photoURL: user?.photoURL,
+          radius: 50,
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        if (user?.displayName != null)
+          Text(
+            user!.displayName!,
+            style: TextStyle(color: Colors.white),
+          ),
+        SizedBox(
+          height: 8,
+        ),
+      ],
+    );
+  }
 }
